6f99ad46322c9d6a32124f2824bfe6d31fa5a327
INsG180609 <santosirade6@gmail.com>
2025-03-11 13:27:40 +0000

    Movimento peças

--- xadrez.c
+++ xadrez.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 int main() {
-   //Mover a torre 5 casas para a direita
+   //Imprime a direção do movimento
    for (int i = 0; i < 5; i++){
-    printf("Direita\n");//imprime a direção do movimento
+    printf("Direita\n");//Mover a torre 5 casas para a direita
+   for (int i = 0; i < 5; i++) 
+    printf("Cima direita\n", i++);// Mover o bispo na diagonal para cima e a direita
+   for (int i = 0; i < 8; i++)
+    printf("Esquerda\n", i++); // Mover a rainha para esquerda
    }
     return 0;
 }
